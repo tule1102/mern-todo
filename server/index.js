f45a40f7e1141035dcc700f729876ad7e4da6b08
tule1102 <tule1102@knights.ucf.edu>
2023-02-02 18:23:58 -0500

    push it

--- server/index.js
+++ server/index.js
@@ -20,26 +20,26 @@ app.use(express.json());
 //     res.send("something son")
 //     res.sendFile('/client/todo-list/build/index.html', {root: '/'});
 // })
-// // const _dirname = path.dirname("");
-// // console.log("rabbit  " + _dirname)
-// // const buildPath = path.join(_dirname, "../client/todo-list/build");
+const _dirname = path.dirname("");
+console.log("_dirname is  " + _dirname)
+const buildPath = path.join(_dirname, "../client/todo-list/build");
 
-// app.use(express.static(buildPath));
+app.use(express.static(buildPath));
 // build on aws
-// console.log("frog " + __dirname)
+console.log("__dirname " + __dirname)
 
-// app.get("*", function(req,res){
-//     res.sendFile(
-//         path.join(__dirname, "../client/todo-list/build/index.html"),
-//         function(err) {
-//             if (err) {
-//                 res.status(500).send(err)
-//                 console.log("Error Danger! " + err)
-//             }
-//         }
-//     )
-//     // res.sendFile('index.html', {root: _dirname})
-// })
+app.get("/*", function(req,res){
+    res.sendFile(
+        path.join(__dirname, "../client/todo-list/build/index.html"),
+        function(err) {
+            if (err) {
+                res.status(500).send(err)
+                console.log("Error Danger! " + err)
+            }
+        }
+    )
+    // res.sendFile('index.html', {root: _dirname})
+})
 
 // app.get("/*", function(req,res){
 //     res.sendFile(
@@ -95,6 +95,7 @@ const PORT = process.env.PORT || 5500;
 const TodoItemRoute = require('./routes/todoItems')
 
 // Lets connect to mongodb
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_CONNECT)
 .then(() => console.log("Database connected"))
 .catch(err => console.log(err))
