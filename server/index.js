bacc9080a0e56d8031f87c361009e74eadb8518b
tule1102 <tule1102@knights.ucf.edu>
2023-02-02 13:10:16 -0500

    traversy way

--- server/index.js
+++ server/index.js
@@ -11,26 +11,33 @@ app.use(cors());
 // allows us us to get data into json format
 app.use(express.json());
 
-const _dirname = path.dirname("");
-console.log("rabbit  " + _dirname)
-const buildPath = path.join(_dirname, "../client/todo-list/build");
 
-app.use(express.static(buildPath));
+// Traversy Media
+
+app.use(express.static('../client/todo-list/build'));
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+})
+// const _dirname = path.dirname("");
+// console.log("rabbit  " + _dirname)
+// const buildPath = path.join(_dirname, "../client/todo-list/build");
+
+// app.use(express.static(buildPath));
 // build on aws
 // console.log("frog " + __dirname)
 
-app.get("*", function(req,res){
-    res.sendFile(
-        path.join(__dirname, "../client/todo-list/build/index.html"),
-        function(err) {
-            if (err) {
-                res.status(500).send(err)
-                console.log("Error Danger! " + err)
-            }
-        }
-    )
-    // res.sendFile('index.html', {root: _dirname})
-})
+// app.get("*", function(req,res){
+//     res.sendFile(
+//         path.join(__dirname, "../client/todo-list/build/index.html"),
+//         function(err) {
+//             if (err) {
+//                 res.status(500).send(err)
+//                 console.log("Error Danger! " + err)
+//             }
+//         }
+//     )
+//     // res.sendFile('index.html', {root: _dirname})
+// })
 
 // app.get("/*", function(req,res){
 //     res.sendFile(
