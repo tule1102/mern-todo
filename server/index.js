b214674c23bcd6f172a9a6e0ddc6552f30e8d72c
tule1102 <tule1102@knights.ucf.edu>
2023-02-02 14:47:39 -0500

    understanding the server response

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static('../client/todo-list/build'));
 app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'))
 })
 // const _dirname = path.dirname("");
 // console.log("rabbit  " + _dirname)
