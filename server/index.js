e9f9ee968370493973effc3cac9aff6452fa189d
tule1102 <tule1102@knights.ucf.edu>
2023-02-02 19:59:25 -0500

    added headers

--- server/index.js
+++ server/index.js
@@ -11,11 +11,10 @@ app.use(cors());
 // allows us us to get data into json format
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
 
 
 // Traversy Media
