ddfa733a56d479ee1301adc127e677c0584cb1c6
tule1102 <tule1102@knights.ucf.edu>
2023-02-05 10:57:36 -0500

    took out the .env push

--- client/todo-list/src/App.js
+++ client/todo-list/src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -7,97 +7,94 @@ function App() {
   const [listItems, setListItems] = useState([]);
   const [isUpdating, setIsUpdating] = useState('');
   const [updateItemText, setUpdateItemText] = useState('');
-  
-  // add new todo item to database
+
+  //add new todo item to database
   const addItem = async (e) => {
     e.preventDefault();
-    try {
-      const res = await axios.post('http://localhost:5500/api/items', {item: itemText})
-      setListItems(prev => [...prev, res.data])
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      setListItems(prev => [...prev, res.data]);
       setItemText('');
-    } catch (error) {
-      console.log(error)
+    }catch(err){
+      console.log(err);
     }
   }
 
-  useEffect(()=> {
+  //Create function to fetch all todo items from database -- we will use useEffect hook
+  useEffect(()=>{
     const getItemsList = async () => {
-      try {
+      try{
         const res = await axios.get('http://localhost:5500/api/items')
-        setListItems(res.data)
-        console.log("like a virgin: " + Array.isArray(listItems))
-
-        console.log("renderss")
-      } catch (err) {
-        console.log(err)
+        setListItems(res.data);
+        console.log('render')
+      }catch(err){
+        console.log(err);
       }
     }
     getItemsList()
-  }, [])
+  },[]);
 
-// Delete Items
-const deleteItem = async (id) => {
-  try {
-    const res = await axios.delete(`http://localhost:5500/api/items/${id}`)
-    const newListItems = listItems.filter(item => item._id !== id);
-    setListItems(newListItems)
-  } catch (error) {
-    console.log(error)
+  // Delete item when click on delete
+  const deleteItem = async (id) => {
+    try{
+      const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+      const newListItems = listItems.filter(item=> item._id !== id);
+      setListItems(newListItems);
+    }catch(err){
+      console.log(err);
+    }
   }
-}
 
-// Update
-const updateItem = async(e) => {
-  e.preventDefault();
-  try {
-    const res = await axios.put(`http://localhost:5500/api/items/${isUpdating}`, {item: updateItemText})
-    console.log(res.data)
-    const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
-    const updatedItem = listItems[updatedItemIndex].item = updateItemText;
-    setUpdateItemText('');
-    setIsUpdating('');
-  } catch (error) {
-    console.log(error)
+  //Update item
+  const updateItem = async (e) => {
+    e.preventDefault()
+    try{
+      const res = await axios.put(`http://localhost:5500/api/item/${isUpdating}`, {item: updateItemText})
+      console.log(res.data)
+      const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
+      const updatedItem = listItems[updatedItemIndex].item = updateItemText;
+      setUpdateItemText('');
+      setIsUpdating('');
+    }catch(err){
+      console.log(err);
+    }
   }
-}
-const renderUpdateForm = () => (
-  <form className="update-form" onSubmit={(e)=> {updateItem(e)}}>
-    <input className='update-new-input' type="text" placeholder="New Item" onChange={e=>{setUpdateItemText(e.target.value)}} value={updateItemText}/>
-    <button className='update-new-btn' type="submit">Update</button>
-  </form>
-)
+  //before updating item we need to show input field where we will create our updated item
+  const renderUpdateForm = () => (
+    <form className="update-form" onSubmit={(e)=>{updateItem(e)}} >
+      <input className="update-new-input" type="text" placeholder="New Item" onChange={e=>{setUpdateItemText(e.target.value)}} value={updateItemText} />
+      <button className="update-new-btn" type="submit">Update</button>
+    </form>
+  )
 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <form className="form" onSubmit={e => addItem(e)}>
-        <input type="text" 
-          placeholder='Add Todo Item' 
-          onChange={e => {setItemText(e.target.value)}} value={itemText}/>
+        <input type="text" placeholder='Add Todo Item' onChange={e => {setItemText(e.target.value)} } value={itemText} />
         <button type="submit">Add</button>
       </form>
       <div className="todo-listItems">
         {
-          // <h1>It works!</h1>
           listItems.map(item => (
           <div className="todo-item">
             {
-              isUpdating === item._id ? renderUpdateForm() :
-              <>
-                <p className="item-content">{item.item}</p>
-                <button className="update-item" onClick={()=>{setIsUpdating(item._id)}}>Update</button>
-                <button className="delete-item" onClick={() => deleteItem(item._id)}>Delete</button>
-              </>
+              isUpdating === item._id
+              ? renderUpdateForm()
+              : <>
+                  <p className="item-content">{item.item}</p>
+                  <button className="update-item" onClick={()=>{setIsUpdating(item._id)}}>Update</button>
+                  <button className="delete-item" onClick={()=>{deleteItem(item._id)}}>Delete</button>
+                </>
             }
           </div>
           ))
         }
         
+
       </div>
     </div>
   );
 }
 
 export default App;
-
-
--- server/index.js
+++ server/index.js
@@ -6,67 +6,25 @@ const path = require('path')
 const app = express();
 
 //user cors
-// app.use(cors());
+app.use(cors());
 // app.use(cors({credentials: true, origin: '*'}))
 
 // allows us us to get data into json format
 app.use(express.json());
 
-// app.use(cors({
-//     origin: 'http://3.142.184.95',
-//     credentials: true
-// }))
+app.use(express.static(path.join(__dirname, "../client/todo-list/build")));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested, Content-Type, Accept Authorization"
-//     )
-//     if (req.method === "OPTIONS") {
-//       res.header(
-//         "Access-Control-Allow-Methods",
-//         "POST, PUT, PATCH, GET, DELETE"
-//       )
-//       return res.status(200).json({})
-//     }
-//     next()
-//   })
+app.get("*", (req, res) =>
+    res.sendFile(
+    path.resolve(__dirname, "../", "client", "todo-list", "build", "index.html")
+    )
+);
 
 
-    app.use(express.static(path.join(__dirname, "../client/todo-list/build")));
-  
-    app.get("*", (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, "../", "client", "todo-list", "build", "index.html")
-      )
-    );
- 
 
-// app.use(cors({credentials: true, origin: 'http://3.142.184.95/5500'}));
-
-// app.use(cors({
-//     origin: 'ec2-3-142-184-95.us-east-2.compute.amazonaws.com/:1',
-//     methods: ['GET', 'POST']
-//   }))
-
-
-  
-// Traversy Media
-
-// app.use(express.static(path.join(__dirname + '../client/todo-list/build')));
-// app.get('/*', (req,res) => {
-//     // res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'))
-//     res.send("something son")
-//     res.sendFile('/client/todo-list/build/index.html', {root: '/'});
-// })
-const _dirname = path.dirname("");
-// console.log("_dirname is  " + _dirname)
-const buildPath = path.join(_dirname, "../client/todo-list/build");
-
-app.use(express.static(buildPath));
-// // build on aws
-// console.log("__dirname " + __dirname)
+// const _dirname = path.dirname("");
+// const buildPath = path.join(_dirname, "../client/todo-list/build");
+// app.use(express.static(buildPath));
 
 // app.get("/*", function(req,res){
 //     res.sendFile(
@@ -78,41 +36,19 @@ app.use(express.static(buildPath));
 //             }
 //         }
 //     )
-//     // res.sendFile('index.html', {root: _dirname})
 // })
 
-// app.get("/*", function(req,res){
-//     res.sendFile(
-//         path.resolve("../client/todo-list/build/index.html", {root: __dirname})
-//         // function(err) {
-//         //     if (err) {
-//         //         res.status(500).send(err)
-//         //     }
-//         // }
-//     )
-//     // res.sendFile('index.html', {root: _dirname})
-// })
+
 
 // const _dirname = path.resolve()
 // app.use(express.static(path.join(_dirname, '../client/todo-list/build')));
 // app.get("*",(req,res) => {
 //   res.sendFile(path.join(_dirname, '../client/todo-list/build/index.html'));
 // })
-// HEROKU
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-  
-//     // This prevents "CANNOT GET /" errors when directly accessing pages from the web.
-//     app.get('*', (req, res) =>
-//     {
-//       console.log("get(*) Entered")
-//       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     });
-//   }
+
+
 
 const PORT = process.env.PORT || 5500;
-
-
 
 // Lets import routes
 const TodoItemRoute = require('./routes/todoItems')
